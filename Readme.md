b88d1e060fbdeabc0bcf10d836937932bf1a0930
devonline <devonline.academy@gmail.com>
2021-08-19 23:56:55 +0300

    Use std maven properties

--- Readme.md
+++
@@ -1,1 +0,0 @@
-# Readme template
---
+++ src/main/java/Launcher.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.SystemUtils;
+
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class Launcher {
+
+    public static void main(final String[] args) {
+        System.out.println(SystemUtils.getJavaHome());
+    }
+}
