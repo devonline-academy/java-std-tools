e20c32eb90f2c80dd299deff0869a37b7f04e84e
devonline <devonline.academy@gmail.com>
2021-08-19 23:12:08 +0300

    Add Launcher class

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,10 @@
+/**
+ * @author devonline
+ * @link http://devonline.academy/java
+ */
+public class Launcher {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello world!");
+    }
+}
