69885895e551693c8aa0bdd61f49342168681300
devonline <devonline.academy@gmail.com>
2021-08-20 11:43:55 +0300

    Add dependency with 'runtime' scope

--- src/main/java/academy/devonline/App.java
+++ src/main/java/academy/devonline/App.java
@@ -1,13 +1,19 @@
 package academy.devonline;
 
-import org.apache.commons.lang3.SystemUtils;
+//import org.apache.commons.lang3.SystemUtils;
+
+import java.lang.reflect.Method;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println(SystemUtils.getJavaHome());
+    public static void main(String[] args) throws Exception {
+        //System.out.println(SystemUtils.getJavaHome());
+
+        Class<?> systemUtilsClass = Class.forName("org.apache.commons.lang3.SystemUtils");
+        Method getJavaHomeMethod = systemUtilsClass.getDeclaredMethod("getJavaHome");
+        System.out.println(getJavaHomeMethod.invoke(null));
     }
 }
