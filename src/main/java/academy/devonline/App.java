1b1ecff7497235e1a713b54c4450b7834a147f59
devonline <devonline.academy@gmail.com>
2021-08-20 11:35:17 +0300

    Add dependency with 'compile' scope

--- src/main/java/academy/devonline/App.java
+++ src/main/java/academy/devonline/App.java
@@ -1,11 +1,13 @@
 package academy.devonline;
 
+import org.apache.commons.lang3.SystemUtils;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(SystemUtils.getJavaHome());
     }
 }
