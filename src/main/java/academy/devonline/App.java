b4b9df041847e2012718fd8bf18330cb16f05931
devonline <devonline.academy@gmail.com>
2021-08-20 11:48:23 +0300

    Simplify example project

--- src/main/java/academy/devonline/App.java
+++ src/main/java/academy/devonline/App.java
@@ -1,13 +1,11 @@
 package academy.devonline;
 
-import org.apache.commons.lang3.SystemUtils;
-
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println(SystemUtils.getJavaHome());
+        System.out.println("Hello world!");
     }
 }
